055921296ef5d8a246f785af52e4e189c0e6ec0f
UlkeshPatil <ulkesh13@gmail.com>
2023-02-27 01:36:43 +0530

    template.py added

---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+
+
+list_of_files = [
+   ".github/workflows/.gitkeep",
+   f"src/__init__.py", 
+   f"src/components/__init__.py", 
+   f"src/utils/__init__.py", 
+   f"src/config/__init__.py", 
+   f"src/pipeline/__init__.py", 
+   f"src/entity/__init__.py", 
+   f"src/constants/__init__.py",
+   f"src/ml/__init__.py",
+   "tests/__init__.py",
+   "configs/config.yaml",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
